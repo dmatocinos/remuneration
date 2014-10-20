cd7cb7d89ad9cc1e818bd51cec1f7108b51382b1
3164 <u61744424@false.(none)>
2014-10-20 11:43:53 +0200

    fix on date UK format

--- public/js/data_entry/client.js
+++ public/js/data_entry/client.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
 
-	$('#period_end_date').datepicker();
-	$('#period_start_date').datepicker();
+	$('#period_end_date').datepicker({ 
+            format: 'dd-mm-yy' 
+    });
+	$('#period_start_date').datepicker({ 
+            format: 'dd-mm-yy' 
+    });
 
 	$('#year_end').datepicker({
 		format: "dd, M"
