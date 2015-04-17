946517880ec5bbcfecc3212b4767ee7a2bc646b0
Dixie Philamerah Atay <dixie.atay@gmail.com>
2015-04-17 10:41:14 +0000

    base.blade.php edited online with Bitbucket

--- main/app/views/layout/base.blade.php
+++ main/app/views/layout/base.blade.php
@@ -5,6 +5,7 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="author" content="Dixie Philamerah J. Atay">
 
     <title>Remuneration Pro</title>
     {{ Asset::container('header')->styles() }}
